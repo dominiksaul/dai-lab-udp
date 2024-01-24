2bdc1a51469aedf555c9bee5319b9dd0e769bed1
Dominik Saul <dominik@saul.ch>
2024-01-24 15:17:52 +0100

    create musician

--- musician/src/main/java/Musician.java
+++ musician/src/main/java/Musician.java
@@ -1,2 +1,19 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Musician {
+    private static final Instrument instrument;
+    public static void main(String[] args) {
+        if (args.length != 1) throw RuntimeException("")
+
+
+        Orchestra orchester = new Orchestra();
+        try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            executor.execute(new TCPServer(orchester));
+            executor.execute(new UDPListener(orchester));
+        } catch (RuntimeException e) {
+            System.out.println("Error with executor: " + e.getMessage());
+        }
+    }
+
 }
