7ba65725db29809883d8a3e5a242d2a7c982e89b
Dominik Saul <dominik@saul.ch>
2024-01-24 15:20:49 +0100

    move files

---
+++ docker/image-musician/src/main/java/Instrument.java
@@ -0,0 +1,21 @@
+public enum Instrument {
+    piano("ti-ta-ti"), trumpet("pouet"), flute("trulu"), violin("gzi-gzi"), drum("boum-boum");
+    private final String sound;
+
+    Instrument(String s) {
+        sound = s;
+    }
+
+    public String sound() {
+        return sound;
+    }
+    static public Instrument getInstrumentWithSound(String sound) {
+        for(var instrument : values()){
+            if (sound.equals(instrument.sound())) {
+                return instrument;
+            }
+        }
+        return null;
+    }
+}
+
